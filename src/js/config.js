b1a97d9d1ecbfd52f8a18c17b85eee2ed928d0ad
Guillermo <memolvpz@gmail.com>
2023-09-15 18:02:00 -0600

    project finished

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '69c3b570-c091-4d3e-af40-ec1b2e0d31f9';
+export const MODAL_CLOSE_SEC = 2.5;
